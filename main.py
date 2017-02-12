6c89f99de7422e569f1edc8286c6a0a36719a223
Szeghi Dorottya <szeghi.d@gmail.com>
2017-02-12 20:56:31 +0100

    "bad request" again but with update

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from models import *
 
 
 app = Flask(__name__)
+app.config.update(dict(
+    SECRET_KEY='development key'
+))
+
 
 @app.route('/')
 def index():
@@ -16,10 +20,13 @@ def list():
     sprinters = Sprinter.select().order_by(Sprinter.id)
     return render_template("list.html", sprinters = sprinters)
 
-@app.route('/story/<id>', methods=['POST', 'GET'])
+@app.route('/story/<id>', methods=['GET','POST'])
 def story(id):
-    print(id)
-    return render_template("form.html")
+    if Sprinter.select().where(Sprinter.id == id).exists():
+        user_story = Sprinter.get(Sprinter.id == id)
+    else:
+        user_story=None
+    return render_template("form.html",user_story=user_story)
 
 @app.route('/delete/<id>')
 def delete(id):
@@ -27,15 +34,41 @@ def delete(id):
     row.delete_instance()
     return redirect(url_for('list'))
 
-@app.route('/add_story', methods=['POST'])
+@app.route('/add_story', methods=['GET','POST'])
 def add_story():
-    Sprinter.create(title=request.form['title'],
-                    story=request.form['story'],
-                    acceptance_criteria=request.form['criteria'],
-                    business_value=request.form['value_in'],
-                    estimation=request.form['estimation_in'],
-                    status=request.form['status'])
+    title=request.form['title']
+    story=request.form['story']
+    ac_cr=request.form['criteria']
+    bu_va=request.form['business_value']
+    est=request.form['estimation']
+    sta=request.form['status']
+    new_story = Sprinter.create(title=title,
+                    story=story,
+                    acceptance_criteria=ac_cr,
+                    business_value=bu_va,
+                    estimation=est,
+                    status=sta)
+    new_story.save()
     flash('New story was successfully posted')
+    return redirect(url_for('list'))
+
+@app.route('/update_story', methods=['GET','POST'])
+def update_story():
+    id = request.form['id']
+    title = request.form['title']
+    story = request.form['story']
+    ac_cr = request.form['criteria']
+    bu_va = request.form['business_value']
+    est = request.form['estimation']
+    sta = request.form['status']
+    user_story=Sprinter.update(id=id,
+                               title=title,
+                               story=story,
+                               acceptance_criteria=ac_cr,
+                               business_value=bu_va,
+                               estimation=est,
+                               status=sta).where(Sprinter.id == id)
+    user_story.execute()
     return redirect(url_for('list'))
 
 if __name__  == "__main__":
